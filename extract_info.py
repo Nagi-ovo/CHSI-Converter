d87c04a372a308f900ecdd9b820966b7cc461891
Nagi-ovo <13264500190@163.com>
2023-06-29 23:14:09 +0800

    feat:translatate

--- extract_info.py
+++ extract_info.py
@@ -1,5 +1,7 @@
 import re
 import PyPDF2
+from pypinyin import lazy_pinyin
+from datetime import datetime
 
 def extract_text_from_pdf(pdf_path):
     pdf_file_obj = open(pdf_path, 'rb')
@@ -16,7 +18,48 @@ def extract_info(patterns_dict, text):
     results = {}
     for prop, pattern in patterns_dict.items():
         match = re.search(pattern, text)
-        results[prop] = match.group(1) if match else None
+        if match:
+            if prop == 'Name':
+                results[prop] = ''.join(lazy_pinyin(match.group(1))).title()
+            elif prop == 'Gender':
+                if match.group(1) == '男':
+                    results[prop] = 'Male'
+                elif match.group(1) == '女':
+                    results[prop] = 'Female'
+                else:
+                    results[prop] = None
+            elif prop == 'Ethnic':
+                value = match.group(1)  
+                pinyin_list = lazy_pinyin(value[:-1])  
+                results[prop] = ''.join(pinyin_list).title()
+            elif prop == 'Date of Birth' or prop == 'Date of Enrollment':
+                value = match.group(1)  
+                parts = value.split('年')
+                year = parts[0]
+                month_day = parts[1].split('月')
+                formatted_date = month_day[0] + '/' + month_day[1].replace('日', '') + '/' + year
+                results[prop] = formatted_date
+            elif prop == 'Levels' :
+                results[prop] = 'Undergraduate'
+            elif prop == 'Form' :
+                results[prop] = 'General full-time remote study'
+            elif prop == 'Educational System':
+                results[prop] = match.group(1)+' years'
+            elif prop == 'Type':
+                results[prop] = 'General higher education'
+            elif prop == 'School Status':
+                value = match.group(1)  # 预计毕业日期：2025年07月01日）
+                date_part = value.split("：")[1]  # 2025年07月01日）
+                parts = date_part.split('年')
+                year = parts[0]
+                month_day = parts[1].split('月')
+                day = month_day[1].replace('日', '').replace('）', '')  # Remove trailing bracket from the day
+                formatted_date = month_day[0] + '/' + day + '/' + year
+                results[prop] = 'Student registration (Expected graduation date: ' + formatted_date + ')'
+            else:
+                results[prop] = match.group(1)
+        else:
+            results[prop] = None
     return results
 
 def extract_info_from_pdf(path):
@@ -48,3 +91,4 @@ def extract_info_from_pdf(path):
     results = extract_info(patterns_dict, text)
     
     return results
+
