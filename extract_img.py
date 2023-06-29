e7972b9bab56cb43f06af3708ce98359a659feb4
Nagi-ovo <13264500190@163.com>
2023-06-29 23:12:44 +0800

    fix:potential bug

--- extract_img.py
+++ extract_img.py
@@ -10,12 +10,13 @@ def extract_image_from_pdf(path, page_number, left, top, right, bottom):
     cropped_image.save(image_path)  # 将截取的图片保存到同一路径下
     return image_path
 
-pdf_path = "教育部学籍在线验证报告_张泽西.pdf"
+'''
+pdf_path = " "
 page_number = 1
 left = 1898  # 左边界坐标
 top = 583  # 上边界坐标
 right = 2230  # 右边界坐标
 bottom = 1026  # 下边界坐标
-
+'''
 
 
